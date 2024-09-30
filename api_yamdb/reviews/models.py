e113ccbb38a60f006159c37516fb1678b5591b6c
Nikita Solovey <solovei292000@gmail.com>
2024-09-30 18:32:26 +0300

    test1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -22,13 +22,4 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, related_name='comments'
-    )
-    post = models.ForeignKey(
-        Post, on_delete=models.SET_NULL, related_name='comments'
-    )
-    text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True
-    )
+    pass
