f959f916b43f146f350efb3238c9a1f24b75b7c0
Bogdan Komandirov <frost0501@gmail.com>
2024-10-01 13:57:51 +0700

    Bogdan 1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .serializers import SignupSerializer
+
+User = get_user_model()
+
+
+class SignupView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
