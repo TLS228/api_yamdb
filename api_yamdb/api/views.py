fd7ee8e216ab2f27b7b2f3c264f339324f4b025f
Bogdan Komandirov <frost0501@gmail.com>
2024-10-01 16:24:43 +0700

    Bogdan 2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,35 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
+from django.core.mail import send_mail
+from rest_framework import serializers, views
 
 from .serializers import SignupSerializer
 
 User = get_user_model()
 
+CONFIRMATION_CODE = '1234567890'
 
-class SignupView(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = SignupSerializer
+
+def get_confirmation_code(nums):
+    confirm_code = ''
+    nums_set = set(nums)
+    for num in nums_set:
+        confirm_code += num
+
+    return int(confirm_code)
+
+
+class SignupView(views.APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            username = serializer.validated_data['username']
+            current_user, status = User.objects.get_or_create(
+                email=email,
+                username=username
+            )
+            current_user.confirmation_code = get_confirmation_code(CONFIRMATION_CODE)
+            current_user.save()
+        else:
+            raise serializers.ValidationError(
+                'Проверьте корректность введённых данных!')
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 CHOICES = (
@@ -10,8 +10,17 @@ CHOICES = (
 
 
 class MyUser(AbstractUser):
-    bio = models.CharField(max_length=256, blank=True)
-    role = models.CharField(max_length=16, choices=CHOICES, default='user')
+    bio = models.CharField(max_length=256, blank=True,
+                           verbose_name='Биография')
+    role = models.CharField(max_length=16, choices=CHOICES,
+                            default='user', verbose_name='Роль')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Title(models.Model):
