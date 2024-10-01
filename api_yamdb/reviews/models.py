93d22867f137cbee3ff301c5afe1cea3cea6753e
Zabavin-Pavel <bosyack105@gmail.com>
2024-10-01 11:46:20 +0300

    model first

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -14,16 +14,49 @@ class MyUser(AbstractUser):
     role = models.CharField(max_length=16, choices=CHOICES, default='user')
 
 
-class Title(models.Model):
-    pass
-
-
 class Category(models.Model):
-    pass
+    name = models.CharField('Категория', max_length=256)
+    slug = models.SlugField('Слаг', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
-    pass
+    name = models.CharField('Жанр', max_length=256)
+    slug = models.SlugField('Слаг', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256)
+    year = models.IntegerField('Год выпуска')
+    description = models.TextField('Описание', blank=True)
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, related_name='genre',
+        null=True, verbose_name='Жанр'
+    )
+    category = models.OneToOneField(
+        Category, on_delete=models.SET_NULL, related_name='category',
+        null=True, verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):  #делаю я
