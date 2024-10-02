d5312e0492f3a25b28e899b1f730ea8100374ce8
Nikita Solovey <solovei292000@gmail.com>
2024-10-02 10:50:25 +0300

    permiss

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,10 +1,32 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import permissions
+
 
 class AdminModeratorAuthor(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
-        if request.method == 'GET':
-            return True
-        if request.user.is_authenticated:
-            if request.user == obj.author or request.user.role in ['admin', 'moderator']:
-                return True
-        return False
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
+        )
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_superuser or request.user.is_admin
+        )
+
+
+class IsReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or request.user.is_authenticated
+        )
